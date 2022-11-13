9383e489ab2fd5e57aea72990c547febd6c1508e
bhavyasreeKavuri <111688424+bhavyasreeKavuri@users.noreply.github.com>
2022-11-13 21:51:00 +0530

    Add files via upload

---
+++ GTASS/Node.java
@@ -0,0 +1,14 @@
+package GTASS;
+
+public class Node {
+		public int data;
+		public Node leftNode;
+		public Node rightNode;
+		
+		public Node(int value) {
+			data = value;
+			leftNode = rightNode = null;
+		}
+	}
+
+
